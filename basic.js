92a82af26485d58a69884dfc73158155a53b6756
hayeen <gracehayeens@gmail.com>
2024-04-01 16:49:17 +0800

    Added and Changed 2 more pages

--- basic.js
+++ basic.js
@@ -4,17 +4,17 @@ $("html").attr("data-bs-theme", "light");
 
 function handleClick() {
   if (localStorage.getItem("theme") !== "light") {
-    console.log("try light");
+    // console.log("try light");
     $("#themedark").addClass("d-none");
     $("#themelight").removeClass("d-none");
     $("html").attr("data-bs-theme", "light");
     localStorage.setItem("theme", "light");
   } else {
-    console.log("try dark");
+    // console.log("try dark");
     $("#themelight").addClass("d-none");
     $("#themedark").removeClass("d-none");
     $("html").attr("data-bs-theme", "dark");
     localStorage.setItem("theme", "dark");
   }
-  console.log("last print");
+  // console.log("last print");
 }
